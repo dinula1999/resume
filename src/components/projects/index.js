feb84f49281847df7c83a8f03ed63968406e9f3a
Dinula Isiwara Jayasekara <dinulaisiwara10@gmail.com>
2021-06-13 12:52:08 +0530

    Make changes to projects section

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -8,7 +8,7 @@ function Projects(){
         <div>
             <h3>Sudoku solver</h3>
             <p>Sudoku solver app made using only Vanilla Javascript.
-                Check it out <a href="https://dinula1999.github.com/sudoku-solver">here</a>
+                Check it out <a href="https://dinula1999.github.io/sudoku-solver">here</a>
             </p>
         </div>
 
